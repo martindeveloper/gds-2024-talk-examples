7a99ac1db2d8e58b00428b5ee5e936b25c2c1907
Martin Pernica <martinpernica@outlook.com>
2024-12-09 15:22:36 +0100

    add symbol generation for odin lib in zig odin interop sample

--- src/zig/zig_odin_interop/build.zig
+++ src/zig/zig_odin_interop/build.zig
@@ -12,17 +12,39 @@ pub fn build(b: *std.Build) void {
         .optimize = optimize,
     });
 
-    const lib_ext = if (builtin.target.os.tag == .windows) "dll" else if (builtin.target.os.tag == .macos) "dylib" else "so";
-    const lib_out_name = (if (builtin.target.os.tag == .macos) "lib" else "") ++ "odin." ++ lib_ext;
+    const lib_build_arg_debug = switch (optimize) {
+        .Debug => "-debug",
+        else => "",
+    };
+
+    const lib_symbols_ext = switch (builtin.target.os.tag) {
+        .windows => "pdb",
+        .macos => "dSYM",
+        else => "",
+    };
+
+    const lib_ext = switch (builtin.target.os.tag) {
+        .windows => "dll",
+        .macos => "dylib",
+        else => "so",
+    };
+
+    const lib_prefix = switch (builtin.target.os.tag) {
+        .macos => "lib",
+        else => "",
+    };
+
+    const lib_out_symbols_filename = lib_prefix ++ "odin." ++ lib_symbols_ext;
+    const lib_out_filename = lib_prefix ++ "odin." ++ lib_ext;
 
     // Compile Odin-based library
-    const odin_lib_cmd = b.addSystemCommand(&.{"odin"});
-    odin_lib_cmd.addArgs(&.{"build"});
-    odin_lib_cmd.addDirectoryArg(b.path("odin_lib/"));
-    odin_lib_cmd.addArgs(&.{ "-build-mode:dynamic", "-no-entry-point", "-out:" ++ lib_out_name });
+    const lib_command = b.addSystemCommand(&.{"odin"});
+    lib_command.addArgs(&.{"build"});
+    lib_command.addDirectoryArg(b.path("odin_lib/"));
+    lib_command.addArgs(&.{ "-build-mode:dynamic", "-no-entry-point", "-out:" ++ lib_out_filename, lib_build_arg_debug });
 
     // Executable depends on the build of the Odin library
-    exe.step.dependOn(&odin_lib_cmd.step);
+    exe.step.dependOn(&lib_command.step);
 
     // Add the current directory to the library path
     // This is necessary to find the dylib at runtime
@@ -38,16 +60,34 @@ pub fn build(b: *std.Build) void {
 
     exe.linkLibC();
 
-    // Copy the dylib to the output directory next to the executable
-    const odin_lib_install = b.addInstallBinFile(b.path(lib_out_name), lib_out_name);
-    odin_lib_install.step.dependOn(&odin_lib_cmd.step);
+    // Copy the lib to the output directory next to the executable
+    const lib_install = b.addInstallBinFile(b.path(lib_out_filename), lib_out_filename);
+    lib_install.step.dependOn(&lib_command.step);
 
     var install_step = b.getInstallStep();
-    install_step.dependOn(&odin_lib_install.step);
+    install_step.dependOn(&lib_install.step);
+
+    if (optimize == .Debug) {
+        switch (builtin.target.os.tag) {
+            .windows => {
+                // On Windows there is PDB file
+                const lib_symbols_install = b.addInstallBinFile(b.path(lib_out_symbols_filename), lib_out_symbols_filename);
+                install_step.dependOn(&lib_symbols_install.step);
+            },
+            .macos => {
+                // On macOS there is .dSYM directory
+                const lib_dsym_install = b.addInstallBinDir(b.path(lib_out_filename ++ ".dSYM"), lib_out_filename ++ ".dSYM");
+                install_step.dependOn(&lib_dsym_install.step);
+            },
+            else => {
+                @panic("Unimplemented OS for debug symbols installation");
+            },
+        }
+    }
 
     b.installArtifact(exe);
 
-    // TODO(martin.pernica): Currently known issue, at least on macOS, that dylib paths will be hardcoded as absolute paths, this seems not happen on Windows
+    // NOTE(martin.pernica): Currently known issue, at least on macOS, that dylib paths will be hardcoded as absolute paths, this seems not happen on Windows
     // You need to hack the dylib paths, not sure how to force Zig build to not hardcode it
     // install_name_tool -change "/Users/martin/Projects/Flying-Rat/gds-2025-samples/src/zig/zig_odin_interop/libodin.dylib" "@rpath/libodin.dylib" zig-out/bin/zig_odin_interop
     // Windows:

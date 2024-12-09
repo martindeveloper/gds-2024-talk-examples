ff53cf3e456817c1dc2cb3e04695ee497fba3dcb
Martin Pernica <martinpernica@outlook.com>
2024-12-09 23:55:55 +0100

    zig odin interop change [*c] to [*:0] sentinel slice and add assert to check for null termination of cstring

--- src/zig/zig_odin_interop/main.zig
+++ src/zig/zig_odin_interop/main.zig
@@ -1,9 +1,10 @@
 const std = @import("std");
+const expect = std.testing.expect;
 
 pub extern fn odin_lib_init() callconv(.C) void;
 pub extern fn odin_lib_destroy() callconv(.C) void;
 pub extern fn odin_fav_num() callconv(.C) u32;
-pub extern fn odin_says_hello() callconv(.C) [*c]u8;
+pub extern fn odin_says_hello() callconv(.C) [*:0]u8;
 
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
@@ -14,7 +15,13 @@ pub fn main() !void {
 
     const odin = odin_fav_num();
     const odin_hello = odin_says_hello();
-    try stdout.print("Odin says: {s}\n", .{odin_hello});
+
+    // Following is not necessary, but we are gonna create sentinel slice of unknown amount from C string
+    // As we used Odin's cstring we know it is null terminated, let's check it
+    const odin_hello_slice = std.mem.span(odin_hello);
+    try expect(odin_hello[odin_hello_slice.len] == 0);
+
+    try stdout.print("Odin says: {s}\n", .{odin_hello_slice});
 
     odin_lib_destroy();
 
